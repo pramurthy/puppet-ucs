6838f3687f0520966c6fd856d2167916701a9bf0
SivaMaplelabs <sivasankar.boomarapu@maplelabs.com>
2017-05-19 11:31:21 +0530

    Modified UCP controller code

--- manifests/docker/set_node_role/ucp_controller_node.pp
+++ manifests/docker/set_node_role/ucp_controller_node.pp
@@ -3,9 +3,11 @@ class ucs::docker::set_node_role::ucp_controller_node(
     $password='maplelabs',
     $user_access_port='19002'
 ){
+
+    $controller_ip=$facts['networking']['ip']
     class { 'docker_ucp':
         controller                => true,
-        host_address              => '10.11.0.39',
+        host_address              => $controller_ip,
         controller_port           => $user_access_port,
         docker_socket_path        => '/var/run/docker.sock',
         license_file              => '/etc/docker/subscription.lic',
