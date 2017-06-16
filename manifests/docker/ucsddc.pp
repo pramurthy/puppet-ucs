697c8dd726431783dfb16865204ae21ad563370e
SivaMaplelabs <sivasankar.boomarapu@maplelabs.com>
2017-06-15 23:37:25 -0700

    Added code for fetching and deploying according to role

---
+++ manifests/docker/ucsddc.pp
@@ -0,0 +1,30 @@
+class ucs::docker::ucsddc{
+
+    $role=$facts['hostname']
+
+    if $role =~ /^ucp-ctrl-(\d+)/ {
+        include ucs::docker::set_node_role::ucp_controller_node
+    }
+
+    elsif $role =~ /^ucp-ctrl-replica-(\d+)/ {
+        include ucs::docker::set_node_role::ucp_controller_replica_node
+    }
+
+    elsif $role =~ /^ucp-dtr-(\d+)/ {
+        include ucs::docker::set_node_role::ucp_dtr_node
+    }
+
+    elsif $role =~ /^ucp-dtr-replica-(\d+)/ {
+        include ucs::docker::set_node_role::ucp_dtr_replica_node
+    }
+
+    elsif $role =~ /^ucp-node-(\d+)/ {
+        include ucs::docker::set_node_role::ucp_worker_node
+    }
+
+    else {
+        notice("Role does not match")
+    }
+
+}
+
