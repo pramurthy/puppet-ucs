8ace0bc816d39c20e938aa7f7d73de9a3417dc0c
SivaMaplelabs <sivasankar.boomarapu@maplelabs.com>
2017-07-30 21:40:25 +0000

    removed the dtr_version param

--- manifests/docker/role/ucp/dtr/replica.pp
+++ manifests/docker/role/ucp/dtr/replica.pp
@@ -1,31 +1,36 @@
 class docker_ee_cvd::docker::role::ucp::dtr::replica(
   $ucp_dtr_node             = undef,
   $ucp_controller_node      = undef,
-  $dtr_version             = $docker_ee_cvd::docker::params::dtr_version,
   $ucp_username            = $docker_ee_cvd::docker::params::ucp_username,
   $ucp_password            = $docker_ee_cvd::docker::params::ucp_password,
 ) inherits docker_ee_cvd::docker::params {
-
-  $dtr_node_ip       = $facts['networking']['ip']
-  $dtr_node_hostname = $facts['networking']['fqdn']
 
   $ucp_ipaddress_query = "facts {
     name = \"ipaddress\" and certname = \"${ucp_controller_node}\" and certname in resources[certname] {
     }
   }"
-  $ucp_ipaddress = puppetdb_query($ucp_ipaddress_query)[0]['value']
 
   $ucp_controller_port_query= "facts {
     name = \"ucp_controller_port\"  and certname = \"${ucp_controller_node}\" and certname in resources[certname] {
     }
   }"
-  $ucp_controller_port = puppetdb_query($ucp_controller_port_query)[0]['value']
 
   $dtr_replica_id_query = "facts {
     name = \"dtr_replica_id\" and certname = \"${ucp_dtr_node}\" and certname in resources[certname] {
     }
   }"
+
+  $dtr_version_query= "facts {
+    name = \"dtr_version\"  and certname = \"${ucp_dtr_node}\" and certname in resources[certname] {
+    }
+  }"
+
+  $dtr_node_ip       = $facts['networking']['ip']
+  $dtr_node_hostname = $facts['networking']['fqdn']
+  $ucp_ipaddress = puppetdb_query($ucp_ipaddress_query)[0]['value']
+  $ucp_controller_port = puppetdb_query($ucp_controller_port_query)[0]['value']
   $dtr_replica_id = puppetdb_query($dtr_replica_id_query)[0]['value']
+  $dtr_version = puppetdb_query($dtr_version_query)[0]['value']
 
   Docker_ee_cvd::Docker::Engine <<| tag == "${ucp_controller_node}" |>>
 
